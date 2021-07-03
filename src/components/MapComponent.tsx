695d6b2fd6fe7ee8fc09bce76d30daaaa6f34e8b
Jantero93 <janzku1993@hotmail.com>
2021-07-03 12:32:43 +0300

    test picture can be added as feature

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -5,13 +5,19 @@ import { useEffect } from 'react';
 import { useRef } from 'react';
 
 import { defaults as defaultControls, FullScreen, ScaleLine } from 'ol/control';
+import Feature from 'ol/Feature';
+import Icon from 'ol/style/Icon';
 import { fromLonLat } from 'ol/proj';
 import { Map } from 'ol';
 import OSM from 'ol/source/OSM';
+import Point from 'ol/geom/Point';
 import TileLayer from 'ol/layer/Tile';
 import { View } from 'ol';
+import Style from 'ol/style/Style';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+
+import testPic from '../map-icons/aaa.png';
 
 import 'ol/ol.css';
 
@@ -21,7 +27,6 @@ const mapStyles = {
 };
 
 export const MapComponent: FunctionComponent = () => {
-  // eslint-disable-next-line
   const [map, setMap] = useState<Map>();
   // eslint-disable-next-line
   const [defaultLayer, setDefaultLayer] = useState<VectorLayer>(new VectorLayer());
@@ -31,7 +36,8 @@ export const MapComponent: FunctionComponent = () => {
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // eslint-disable-next-line
+    defaultLayer.setSource(defaultSource);
+
     const map: Map = new Map({
       controls: defaultControls().extend([
         new FullScreen(),
@@ -48,29 +54,54 @@ export const MapComponent: FunctionComponent = () => {
         defaultLayer
       ],
       view: new View({
-        center: fromLonLat([23.8, 61.6]),
-        zoom: 9
+        ///  center: fromLonLat([23.8, 61.6]),
+        center: [0, 0],
+        zoom: 3
       })
     });
-
     setMap(map);
+    // eslint-disable-next-line
   }, []);
 
   const testButtonClicked = (): void => {
     map!.setView(
       new View({
         center: fromLonLat([-73.95, 40.7]),
-        zoom: 9 
+        zoom: 9
       })
     );
+    map!.setTarget(mapRef.current as HTMLElement);
+  };
+
+  const addFeatureButtonClicked = (): void => {
+    const newFeature: Feature = new Feature({
+      geometry: new Point([0, 0]),
+      name: 'testi nimi'
+    });
+
+    newFeature.setId(0);
+
+    console.log(`testPic`, testPic);
+
+    const featureStyle: Style = new Style({
+      image: new Icon({
+        src: testPic,
+        anchor: [0, 0],
+        size: [32, 32]
+      })
+    });
+
+    newFeature.setStyle(featureStyle);
+    defaultSource.addFeature(newFeature);
+
+    console.log(defaultLayer.getSource().getFeatureById(0));
   };
 
   return (
-    <>
-      <div id="map-root">
-        <button onClick={testButtonClicked}>Test Btn</button>
-        <div id="map" ref={mapRef} style={mapStyles} />
-      </div>
-    </>
+    <div id="map-root">
+      <button onClick={testButtonClicked}>Test Btn</button>
+      <button onClick={addFeatureButtonClicked}>Add Feature</button>
+      <div id="map" ref={mapRef} style={mapStyles} />
+    </div>
   );
 };
