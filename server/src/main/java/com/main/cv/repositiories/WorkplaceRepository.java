a4fd39da7f8dcf5aaf31f22467d1961d58c4754e
Jantero93 <janzku1993@hotmail.com>
2021-10-13 16:46:20 +0300

    solved n + 1 and live reload enabled

--- server/src/main/java/com/main/cv/repositiories/WorkplaceRepository.java
+++ server/src/main/java/com/main/cv/repositiories/WorkplaceRepository.java
@@ -1,7 +1,14 @@
 package com.main.cv.repositiories;
 
 import com.main.cv.items.Workplace;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkplaceRepository extends JpaRepository<Workplace, Long> {
+
+    /* Prevent N+1 when fetching location with workplace */
+    @EntityGraph(attributePaths = {"location"} )
+    List<Workplace> findAll();
 }
