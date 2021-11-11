90fd43158506ed7687191dfced27598cda55f18f
Jantero93 <janzku1993@hotmail.com>
2021-11-11 20:02:22 +0200

    thymeleaf, render side index.html, Procfile

--- server/src/main/java/com/main/cv/controllers/DefaultController.java
+++ server/src/main/java/com/main/cv/controllers/DefaultController.java
@@ -1,13 +1,13 @@
 package com.main.cv.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DefaultController {
 
     @GetMapping("/")
-    public String testPage() {
-        return "something stuff on test page root";
+    public String index() {
+        return "index";
     }
 }
