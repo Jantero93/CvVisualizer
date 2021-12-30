eb8f96745afd632751c94ba46852ce69ff86e1d7
Jantero93 <janzku1993@hotmail.com>
2021-12-30 10:12:23 +0200

    test auto deploy

--- src/main/client/src/App.tsx
+++ src/main/client/src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setWorkPlaces } from './store/actions/mainDataActions';
 
-/** Components */
+/** Components  */
 import Header from './components/Header';
 import Homepage from './components/Homepage';
 import MapComponent from './components/MapComponent';
